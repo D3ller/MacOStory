4e4d2724aed7594ffd80baa1184400c5620301a6
EmilienRozier <emilienrozier@icloud.com>
2024-04-03 15:21:41 +0200

    imessage

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import IMessage from "@/views/IMessage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+    {
+      path:'/imessage',
+      name: 'imessage',
+      component: IMessage
+    }
   ]
 })
 
