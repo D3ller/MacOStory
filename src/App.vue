e2953d3b3b3f47a338d1cdfb06d6da11a512d4c2
EmilienRozier <emilienrozier@icloud.com>
2024-04-04 16:00:06 +0200

    notif

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Top from "@/components/Top.vue";
+import Notif from "@/components/Notif.vue"
 </script>
 
 <template>
 
   <div class="flex w-full justify-center">
   <div class="ubuntu min-w-[380px]">
+    <Notif nom="test" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. A adipisci assumenda commodi consectetur dignissimos enim eos impedit minima necessitatibus nemo nihil officia optio placeat, porro quaerat saepe sapiente, suscipit tempora?"></Notif>
     <Top></Top>
   <RouterView />
   </div>
---
+++ src/components/Notif.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex justify-center w-full mt-3 absolute z-10 font-Poppins notif">
+    <div class="flex w-4/5 h-20 bg-blur rounded-2xl shadow-2xl">
+      <div class="m-2 flex items-center">
+        <img src="/app/message.png" class="w-16">
+        <div class="h-full m-1">
+          <h5>{{nom}}</h5>
+          <div class="overflow-hidden h-10">
+            <p class="text-sm">{{content}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.bg-blur {
+  -webkit-backdrop-filter: blur(15px); /* assure la compatibilité avec safari */
+  backdrop-filter: blur(15px);
+  background-color: rgba(190, 190, 190, 0.2);
+}
+
+.notif {
+  overflow-y: hidden;
+  transform: translateY(-200px);
+  transition: 1s;
+}
+
+.active {
+  transform: translateY(0);
+}
+
+</style>
+
+<script setup>
+const props = defineProps(["nom", 'content'])
+
+</script>
